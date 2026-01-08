b7fa67bc4cec05d826d671e39b9731f6ad4e943d
AD1279 <ad1279@student.labranet.jamk.fi>
2026-01-08 13:47:21 +0200

    footer layout change

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -7,8 +7,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="border-t bottom-0">
-      <div className="grid grid-cols-3 mx-auto max-w-5xl px-6 py-6 text-sm text-gray-300">
+      <div className="grid grid-cols-2 mx-auto max-w-5xl px-10 py-6 text-sm text-gray-300">
         <div className="text-left">
+          <p>Designed & built by Aar00t</p>
+          <br />
           <p>© {new Date().getFullYear()} All rights reserved.</p>
                     {/* Privacy Notice link */}
           <Link 
@@ -19,13 +21,8 @@ export default function Footer() {
           </Link>
 
         </div>
-        <div className="text-center">
-          <p>Contact: aaro.nuoramo@gmail.com</p>
-
-
-        </div>
         <div className="text-right">
-          <p>Designed & built by Aar00t</p>
+          <p>Contact: aaro.nuoramo@gmail.com</p>
 
 
         </div>
